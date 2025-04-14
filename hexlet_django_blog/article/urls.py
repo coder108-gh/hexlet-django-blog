801b97cc9a97629dcc56dffa56126f24fbc49d82
coder108 <eugene_kumin@mail.ru>
2025-04-14 13:55:03 +0300

    inherit templates

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,7 @@
 from django.urls import  path
 from hexlet_django_blog.article import  views
 # from hexlet_django_blog.urls import urlpatterns
-
+app_name = 'myapp'
 urlpatterns = [
     path("", views.index)
 ]
